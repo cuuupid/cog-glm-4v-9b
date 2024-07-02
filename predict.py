18978ac408236f7286b2f48c684d8dcd410cde70
💔 <shah@priansh.com>
2024-07-02 00:44:57 -0400

    v1

---
+++ predict.py
@@ -0,0 +1,39 @@
+from cog import BasePredictor, Path, Input
+import torch
+from PIL import Image
+from transformers import AutoModel, AutoTokenizer
+from pget import pget_manifest
+
+
+class Predictor(BasePredictor):
+
+    def setup(self):
+        pget_manifest()
+        self.device = "cuda"
+        self.tokenizer = AutoTokenizer.from_pretrained("./weights", trust_remote_code=True)
+        self.model = (
+            AutoModel.from_pretrained("./weights",
+                torch_dtype=torch.bfloat16,
+                low_cpu_mem_usage=True,
+                trust_remote_code=True,
+            )
+            .to(self.device)
+            .eval()
+        )
+
+    def predict(self,
+        image: Path = Input(description="Image input"),
+        prompt: str = Input(description="Propmt")
+    ) -> str:
+        image = Image.open(image).convert("RGB")
+        inputs = self.tokenizer.apply_chat_template(
+            [{"role": "user", "image": image, "content": prompt}],
+            add_generation_prompt=True,
+            tokenize=True,
+            return_tensors="pt",
+            return_dict=True,
+        ).to(self.device)
+        with torch.no_grad():
+            outputs = self.model.generate(**inputs, max_length=2500, do_sample=True, top_k=1)
+            outputs = outputs[:, inputs["input_ids"].shape[1] :]
+            return self.tokenizer.decode(outputs[0])
